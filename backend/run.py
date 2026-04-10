a519c71c88f5ddbfaaa3446977c8aa71393e294f
anshpatoliya27 <anshpatoliya8@gmail.com>
2026-04-11 03:24:08 +0530

    edit file

--- backend/run.py
+++ backend/run.py
@@ -2,5 +2,4 @@ from app.main import app
 
 if __name__ == "__main__":
     print("[*] Starting SmartCommerceAI on http://127.0.0.1:8000")
-    # Using the native Flask runner to avoid ASGI/WSGI signature errors
-    app.run(host="127.0.0.1", port=8000, debug=True)
+
