2d2e260b2c0a98e2e43222ba001b9dcfb06cee8a
anshpatoliya27 <anshpatoliya8@gmail.com>
2026-04-11 04:37:34 +0530

    add

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,38 @@
+# from flask import Flask
+# from flask_cors import CORS
+# from app.extensions import init_db
+# from dotenv import load_dotenv
+# from app.routes.auth_routes import auth_bp
+# from app.routes.api_routes import api_bp
+
+# load_dotenv()
+
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)
+
+#     init_db(app)
+
+#     app.register_blueprint(auth_bp, url_prefix="/auth")
+#     app.register_blueprint(api_bp)
+
+#     @app.route("/")
+#     def home():
+#         return {"message": "API Running"}
+
+#     return app
+
+# app = create_app()
+
+
+
+
 from flask import Flask
-from flask_cors import CORS
-from app.extensions import init_db
-from dotenv import load_dotenv
-from app.routes.auth_routes import auth_bp
-from app.routes.api_routes import api_bp
 
-load_dotenv()
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
+# register your blueprints here
+# app.register_blueprint(auth_bp)
 
-    init_db(app)
-
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(api_bp)
-
-    @app.route("/")
-    def home():
-        return {"message": "API Running"}
-
-    return app
-
-app = create_app()
+# ❌ REMOVE THIS:
+# app.run(...)
